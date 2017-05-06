815de5f65c0502dd43e9e6b1c1bb488a16a05179
Juha Kalamies <juhakalamies@Juhas-MacBook-Air.local>
2017-05-06 23:23:41 +0300

    More tuning heroku config vars

--- server.js
+++ server.js
@@ -14,9 +14,10 @@ let app = express();
 
 // Initialize
 // let serviceAccount = require('./SECRET_acc/serviceAccountKey.json');
-let serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+// let serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
 firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
   databaseURL: "https://housing-98e93.firebaseio.com"
 });
 let ref = firebase.database().ref('housing/sold');
